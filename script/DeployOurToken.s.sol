4c92fbc8eeb80c93c5362d23e5c638b36e41800c
Jason <Jason@iMac.com>
2024-01-04 18:50:05 +0200

    Our first erc20!

---
+++ script/DeployOurToken.s.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {OurToken} from "../src/OurToken.sol";
+
+contract DeployOurToken is Script {
+    uint256 public constant INTIAL_SUPPLY = 1000 ether;
+
+    function run() external returns(OurToken){
+        vm.startBroadcast();
+
+        OurToken ot = new OurToken(INTIAL_SUPPLY);
+        vm.stopBroadcast();
+        return ot; 
+    }
+}
